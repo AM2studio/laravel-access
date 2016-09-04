4d125d06d0dadf970b5ba7faec4cd996d2fe3698
Marko Samec <marko.schamec@gmail.com>
2016-09-04 14:38:15 +0200

    feature(package): skeleton app

---
+++ src/AM2StudioLaravelAccessServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AM2Studio\LaravelAccess;
+
+use Illuminate\Support\ServiceProvider;
+
+class AM2StudioLaravelAccessServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}

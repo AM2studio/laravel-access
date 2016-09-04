5dc3239dc53d4146acf771afa102a559571d3e8c
Marko Samec <marko.schamec@gmail.com>
2016-09-04 15:15:56 +0200

    refactor(tables): added model_id to pivot tables

--- src/migrations/2016_09_04_124048_create_role_permission_table.php
+++ src/migrations/2016_09_04_124048_create_role_permission_table.php
@@ -25,6 +25,7 @@ class CreateRolePermissionTable extends Migration
                 ->references('id')->on('permissions')
                 ->onDelete('restrict')
                 ->onUpdate('cascade');
+            $table->unsignedInteger('model_id');
             $table->timestamps();
         });
     }

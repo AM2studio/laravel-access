796b0465ebb51c6b6d0c68b6da4eba857f5fe0b1
Marko Samec <marko.schamec@gmail.com>
2016-09-04 15:14:07 +0200

    feature(models): created database models

---
+++ src/UserPermission.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AM2Studio\LaravelAccess;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserPermission extends Model  {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_permission';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'permission_id'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [];
+
+}

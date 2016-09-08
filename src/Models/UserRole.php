82b97c353fba1a464dc9d3eb502e54606d470283
Marko Samec <marko.schamec@gmail.com>
2016-09-08 15:13:50 +0200

    feature(migration): added user_role table and model

---
+++ src/Models/UserRole.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AM2Studio\LaravelAccess\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserRole extends Model  {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_role';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'role_id', 'model_id'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [];
+
+}

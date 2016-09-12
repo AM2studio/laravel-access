c85e3aea811edba502c79206bccf41952b42610a
Marko Samec <marko.schamec@gmail.com>
2016-09-12 15:46:53 +0200

    refactor(role): roles moved out of equation for now

--- src/AM2StudioLaravelAccess.php
+++ src/AM2StudioLaravelAccess.php
@@ -3,20 +3,10 @@
 namespace AM2Studio\LaravelAccess;
 
 use AM2Studio\LaravelAccess\Models\Permission;
-use AM2Studio\LaravelAccess\Models\Role;
 use Illuminate\Database\Eloquent\Model;
 
 class AM2StudioLaravelAccess
 {
-    public function roleHasModel($role, $model = null)
-    {
-        $role = Role::find($role);
-        if($role->model != $this->getClassName($model)) {
-            return false;
-        }
-        return true;
-    }
-
     public function permissionHasModel($permission, $model = null)
     {
         $permission = Permission::find($permission);
--- src/Traits/UserAccessTrait.php
+++ src/Traits/UserAccessTrait.php
@@ -4,44 +4,16 @@ namespace AM2Studio\LaravelAccess\Traits;
 
 use AM2Studio\LaravelAccess\Access;
 use AM2Studio\LaravelAccess\Models\Permission;
-use AM2Studio\LaravelAccess\Models\Role;
 use Illuminate\Database\Eloquent\Model;
 use InvalidArgumentException;
 
-trait HasRolesAndPermissions
+trait UserAccessTrait
 {
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class, 'user_role', 'user_id', 'role_id')->withTimestamps();
-    }
-
     public function permissions()
     {
         return $this->belongsToMany(Permission::class, 'user_permission', 'user_id', 'permission_id')
             ->withTimestamps()
             ->withPivot('model_id');
-    }
-
-    public function attachRole($role, $model = null)
-    {
-        $attributes = [];
-
-        if(!Access::roleHasModel($role, $model)) {
-            throw new InvalidArgumentException('Selected role does not match selected model!');
-        }
-
-        if($model instanceof Model) {
-            $attributes['model_id'] = $model->getKey();
-        }
-
-        return (!$this->hasRole($role, $model)) ? $this->roles()->attach($role, $attributes) : true;
-    }
-
-    public function hasRole($role, $model = null)
-    {
-        return $this->roles->contains(function($value, $key) use ($role, $model){
-            return ($role == $value->id || str_is($role, $value->slug)) && Access::getClassName($model);
-        });
     }
 
     public function attachPermission($permission, $model = null)
